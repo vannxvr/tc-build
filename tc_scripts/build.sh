vannxvr/tc-build

commit 55ef65bf6caf78f38fb7f426779288248c6bd219

tc_scripts: Stop updating LLVM repository
Change-Id: Iee7980b0de6cf6e26a64a67dec695008e3fa4b3c

tc_scripts/build.sh
@@ -18,7 +18,7 @@ kecho "Building Clang LLVM (step: ${1})..."
         --projects clang lld \
         --pgo llvm \
         --quiet-cmake \
-        --shallow-clone \
+        --no-update \
         --targets ARM AArch64 X86 \
         --vendor-string "greenforce" 2>&1 | tee "${llvm_log}"
 

commit ba2fdc486b0e06f244f13259c9fbc789ec2643d4

tc_scripts: Avoid overwriting in the llvm-project repository
This can confidently pass '--ref' and '--use-good-revision' without doing anything.

fixes: 55ef65bf ("tc_scripts: Stop updating LLVM repository")
Change-Id: Ibe8444fddc863ceb5648d12b8f3ea8f0bd52ab89

tc_scripts/build.sh
@@ -17,10 +17,10 @@ kecho "Building Clang LLVM (step: ${1})..."
         --install-folder "${install_path}" \
         --install-target distribution \
         --projects clang lld \
+        --llvm-folder "${DIR}/src/llvm-project" \
         --lto thin \
         --pgo llvm \
         --quiet-cmake \
-        --no-update \
         --targets ARM AArch64 X86 \
         --vendor-string "greenforce" 2>&1 | tee "${llvm_log}"
 
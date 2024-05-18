3f2858cbe51b989b336dd867067ca494f0477b34
fadlyas07 <mhmmdfdlyas@proton.me>
2024-05-18 10:50:51 +0000

    tc_scripts: Stop modifying README.md
    Since the introduction of the alternative script for downloading the toolchain, this is no longer relevant as everything needed is already available there.
    
    Change-Id: Iebd8cf39c7c1d423941130d63e20b83a798ef93b

--- tc_scripts/info.sh
+++ tc_scripts/info.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2023-2024 fadlyas07 <mhmmdfdlyas@proton.me>
 
 # Remove existing files
-rm -rf README.md latest*
+rm -rf latest*
 
 # Create latest.txt and populate it with release tag
 echo -e "[tag]\n${release_tag}" >latest.txt
@@ -60,49 +60,3 @@ touch latest_url.txt
     echo -e "# It serves as a reference for accessing the most recent version of Clang for use in various projects.\n"
     echo "latest_url=${release_url}"
 } >latest_url.txt
-
-# Create README.md file and populate it with content
-touch README.md
-{
-    echo -e "# Greenforce Clang\n"
-    echo -e "## Host compatibility\n"
-    echo -e "This toolchain is built on ${distro_image}, which uses glibc ${glibc_version}. Compatibility with older distributions cannot be guaranteed. Other libc implementations (such as musl) are not supported.\n"
-    echo -e "## Building Linux\n"
-    echo -e "This is how you start initializing the Greenforce Clang to your server, use a command like this:\n"
-    echo -e '```bash'
-    echo -e "wget -c ${release_url} -O - | tar --use-compress-program=unzstd -xf - -C ~/greenforce-clang\n"
-    echo -e '```\n'
-    echo -e "Clone GCC (AArch64) for binutils:\n"
-    echo -e '```bash'
-    echo -e 'git clone https://github.com/greenforce-project/gcc-arm64 -b main ~/gcc64 --depth=1\n'
-    echo -e '```\n'
-    echo -e "Clone GCC (Arm32) **If your kernel has a 32-bit vDSO**:\n"
-    echo -e '```bash\n'
-    echo -e 'git clone https://github.com/greenforce-project/gcc-arm64 -b main ~/gcc32 --depth=1\n'
-    echo -e '```\n'
-    echo -e 'Make sure you have this toolchain in your `PATH`:\n'
-    echo -e '```bash\n'
-    echo -e 'export PATH="~/greenforce-clang/bin:~/gcc64/bin:~/gcc32/bin:${PATH}"\n'
-    echo -e '```\n'
-    echo -e 'For an AArch64 cross-compilation setup, you must set the following variables. Some of them can be environment variables, but some must be passed directly to `make` as a command-line argument. It is recommended to pass **all** of them as `make` arguments to avoid confusing errors:\n'
-    echo -e '- `CC=clang` (must be passed directly to `make`)'
-    echo -e '- `CROSS_COMPILE=aarch64-linux-gnu-`'
-    echo -e '- For 32-bit vDSO: `CROSS_COMPILE_ARM32=arm-linux-gnueabi-`\n'
-    echo -e 'Optionally, you can also choose to use as many LLVM tools as possible to reduce reliance on binutils. All of these must be passed directly to `make`:\n'
-    echo -e '- `AR=llvm-ar`'
-    echo -e '- `NM=llvm-nm`'
-    echo -e '- `OBJCOPY=llvm-objcopy`'
-    echo -e '- `OBJDUMP=llvm-objdump`'
-    echo -e '- `STRIP=llvm-strip`\n'
-    echo -e 'Note, however, that additional kernel patches may be required for these LLVM tools to work. It is also possible to replace the binutils linkers (`lf.bfd` and `ld.gold`) with `lld` and use Clangs integrated assembler for inline assembly in C code, but that will require many more kernel patches and it is currently impossible to use the integrated assembler for *all* assembly code in the kernel.\n'
-    echo -e "Android kernels older than 4.14 will require patches for compiling with any Clang toolchain to work; those patches are out of the scope of this project. See [android-kernel-clang](https://github.com/nathanchance/android-kernel-clang) for more information.\n"
-    echo -e 'Android kernels 4.19 and newer use the upstream variable `CROSS_COMPILE_COMPAT`. When building these kernels, replace `CROSS_COMPILE_ARM32` in your commands and scripts with `CROSS_COMPILE_COMPAT`.\n'
-    echo -e "### Differences from other toolchains\n"
-    echo -e "Greenforce Clang has been designed to be easy-to-use compared to other toolchains, such as [AOSP Clang](https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/). The differences are as follows:\n"
-    echo -e '- `CLANG_TRIPLE` does not need to be set because we dont use AOSP binutils.'
-    echo -e '- `LD_LIBRARY_PATH` does not need to be set because we set library load paths in the toolchain.'
-} >README.md
-
-# Fixing typos and grammar
-sed -i "s/Clangs/Clang's/g" README.md
-sed -i "s/dont/don't/g" README.md

53687956f74b4a29b1e45faef9265cd05e2bd4b9
fadlyas07 <mhmmdfdlyas@proton.me>
2024-07-24 12:23:09 +0700

    tc_scripts: Sync to https://github.com/greenforce-project/greenforce_clang/commit/9d96f5e056eb918078834021038edd68c765f361
    Change-Id: I0f08cdeb7783bd0662cdc26633602dda6206f35f

--- tc_scripts/info.sh
+++ tc_scripts/info.sh
@@ -3,7 +3,8 @@
 # Copyright (C) 2023-2024 fadlyas07 <mhmmdfdlyas@proton.me>
 
 echo -e "[tag]\n${release_tag}" >latest.txt
-touch "${short_clang}-${release_tag}-info.txt"
+release_url="https://github.com/greenforce-project/greenforce_clang/releases/download/${release_tag}/${release_file}"
+touch "clang_notes.txt"
 {
     echo -e "[date]\n${release_date}\n"
     echo -e "[clang-ver]\n${clang_version}\n"
@@ -12,12 +13,13 @@ touch "${short_clang}-${release_tag}-info.txt"
     echo -e "[llvm-commit-msg]\n${lcommit_message}\n"
     echo -e "[host-glibc]\n${glibc_version}\n"
     echo -e "[size]\n${release_size}\n"
-    echo "[shasum]\n${release_shasum}"
-} >"${short_clang}-${release_tag}-info.txt"
+    echo -e "[link-rel]\n${release_url}\n"
+    echo -e "[shasum]\n${release_shasum}"
+} >"clang_notes.txt"
 
 touch /tmp/commit_msg
 {
-    echo -e "greenforce: Bump to ${release_tag} build\n"
+    echo -e "CI: Bump to ${release_tag} build\n"
     echo "Clang version: ${clang_version}"
     echo "LLD version: ${lld_version}"
     echo "LLVM repo commit: ${lcommit_message}"
@@ -27,5 +29,5 @@ touch /tmp/commit_msg
 touch get_latest_url.sh
 {
     echo -e "#!/usr/bin/env bash\n"
-    echo "LATEST_URL=https://github.com/greenforce-project/greenforce_clang/releases/download/${release_tag}/${release_file}"
+    echo "LATEST_URL=${release_url}"
 } >get_latest_url.sh

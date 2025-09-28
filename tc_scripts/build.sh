a298c15166dad59952e99c3f2b3c4a83acd39272
vannxavero <vannxaveroo@proton.me>
2025-09-28 23:28:07 +0700

    Change Vendor Strings
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- tc_scripts/build.sh
+++ tc_scripts/build.sh
@@ -17,7 +17,7 @@ export llvm_log="${DIR}/build-llvm-${release_tag}.log"
     --pgo llvm \
     --quiet-cmake \
     --targets ARM AArch64 X86 \
-    --vendor-string "Gf" 2>&1 | tee "${llvm_log}"
+    --vendor-string "DiscussionVersePrjkt" 2>&1 | tee "${llvm_log}"
 
 for clang in "${install_path}"/bin/clang; do
     if ! [[ -f "${clang}" || -f "${DIR}/build/llvm/instrumented/profdata.prof" ]]; then
